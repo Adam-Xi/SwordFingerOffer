64eb23131e67bc1394f5b0781e5d5ef4ab43ed1e
Adam-Xi <321041835@qq.com>
2020-03-16 00:00:11 +0800

    数值的整数此房

---
+++ Power.cc
@@ -0,0 +1,26 @@
+/*
+题目描述
+给定一个double类型的浮点数base和int类型的整数exponent。
+求base的exponent次方。
+保证base和exponent不同时为0
+*/
+
+class Solution {
+public:
+    double Power(double base, int exponent) {
+        //return pow(base, exponent);
+        long long p = abs((long long)exponent);
+        double res = 1.0;
+        while(p)
+        {
+            if(p & 1)
+            {
+                //在指数循环右移的过程中，始终都会存在奇数次幂，执行当前步骤
+                res *= base;  //当指数不为0时，将结果保存在res中
+            }
+            base *= base;
+            p >>= 1;
+        }
+        return exponent < 0 ? 1 / res : res;
+    }
+};
